3e9202150fe1e070fcdc8d9deca261e0cd0843c5
Varshithaa-K <112675193+Varshithaa-K@users.noreply.github.com>
2023-08-05 20:14:37 +0530

    Create baier_robertson.py

---
+++ PET_Values/baier_robertson.py
@@ -0,0 +1,10 @@
+#Baier-Robertson Code
+
+def baier_robertson(T,ters_rad):
+  Ra=ters_rad
+  TD=6
+  PET=0.157*T+0.158*TD+0.109*Ra-5.39
+  return PET
+
+df['PET_BR']=baier_robertson(df['TA_F'],df['LW_OUT']-df['LW_IN_F'])
+df['PET_BR']
